59baf7c1b3d4a129b76b12edd2ead85b771a354f
eas <a.elovikov@gmail.com>
2009-10-11 18:59:52 +0700

    Added random color

--- colors.h
+++ colors.h
@@ -1,4 +1,6 @@
 #pragma once
+#include <stdlib.h>
+
 namespace Colors
 {
 	const DWORD Black =		D3DCOLOR_XRGB(000, 000, 000);
@@ -10,4 +12,9 @@ namespace Colors
 	const DWORD Magenta =	D3DCOLOR_XRGB(255, 000, 255);
 	const DWORD White =		D3DCOLOR_XRGB(255, 255, 255);
 	const DWORD Gray =		D3DCOLOR_XRGB(128, 128, 128);
+
+	inline DWORD Random()
+	{
+		return D3DCOLOR_XRGB(rand()%256, rand()%256, rand()%256);
+	}
 } //namespace Colors
